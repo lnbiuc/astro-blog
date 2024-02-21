1a2036aa054c0e14e70bff56e7c0fa0b16b41f6b
ayanamirei <violetzzs@proton.me>
2024-02-21 22:26:18 +0800

    :tada:

---
+++ scripts/menu.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+
+if (hamburger !== null) {
+	hamburger.addEventListener('click', () => {
+		const links = document.querySelector('.nav-links');
+
+		if (links !== null) {
+			links.classList.toggle('expanded');
+		}
+	});
+}
